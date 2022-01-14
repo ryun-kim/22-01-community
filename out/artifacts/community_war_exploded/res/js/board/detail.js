4a35a5d3bc1a91dd5aac4c82543b966ed67ce699
ryun <kjr6260@naver.com>
2022-01-14 16:11:16 +0900

    22-01-14 myFetch 고도화 (2)

--- out/artifacts/community_war_exploded/res/js/board/detail.js
+++ out/artifacts/community_war_exploded/res/js/board/detail.js
@@ -25,6 +25,12 @@
 
     const cmtFrmElem = document.querySelector('#cmtFrm');
     if(cmtFrmElem) { // true: 로그인 한 상태
+
+        //input-text ctnt에서 엔터치면 submit날아가기 때문에 막는다.
+        cmtFrmElem.addEventListener('submit', (e)=> {
+            e.preventDefault();
+        });
+
         cmtFrmElem.btn_submit.addEventListener('click', () => {
             const cmtVal = cmtFrmElem.ctnt.value;
             if(cmtVal.length === 0) {
@@ -37,21 +43,13 @@
         });
 
         const insBoardCmtAjax = (val) => {
-            fetch('/board/cmt', {
-                'method': 'post',
-                'headers': { 'Content-Type': 'application/json' },
-                'body': JSON.stringify({
-                    'iboard': dataElem.dataset.iboard,
-                    'ctnt': val
-                })
-            })
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data);
-                })
-                .catch(err => {
-                    console.log(err);
-                });
+            const param = {
+                'iboard': dataElem.dataset.iboard,
+                'ctnt': val
+            };
+            myFetch.post('/board/cmt', (data) => {
+                console.log(data);
+            }, param);
         }
     }
 }
--- out/artifacts/community_war_exploded/res/js/common.js
+++ out/artifacts/community_war_exploded/res/js/common.js
@@ -3,7 +3,7 @@ const msg = {
     fnIsDel : function(target) {
         return `${target}을(를) ` + this.isDel;
     }
-}
+};
 
 //정규식 테스트 사이트
 //https://www.regextester.com/
@@ -17,9 +17,32 @@ const regex = {
         id: '대소문자_숫자조합으로 4~15글자',
         pw: '대소문자+숫자+!@_ 조합으로 4~20글자',
         nm: '한글조합으로 2~5글자',
-        ctnt: '<, > 는 사용할 수 없습니다.',
+        ctnt: '<, >는 사용할 수 없습니다.',
     },
     isWrongWith: function(target, val) {
         return (target && val) ? !this[target].test(val) : true;
     }
 };
+
+const myFetch = {
+    send: function(fetchObj, cb) {
+        return fetchObj
+            .then(res => res.json())
+            .then(cb)
+            .catch(e => { console.log(e) });
+    },
+    get: function(url, cb, param) {
+        if(param) {
+            const queryString = Object.keys(param).map(key => `${key}=${param[key]}`).join('&');
+            url = `${url}?${queryString}`;
+        }
+        return this.send(fetch(url), cb);
+    },
+    post: function(url, cb, param) {
+        return this.send(fetch(url, {
+            'method': 'post',
+            'headers': { 'Content-Type': 'application/json' },
+            'body': JSON.stringify(param)
+        }), cb);
+    }
+}
